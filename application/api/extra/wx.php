9395216104b8a74a0b8d719660c2eec6ab47b9c4
lining <1366435203@qq.com>
2018-05-27 18:52:55 +0800

    ignore

--- application/api/extra/wx.php
+++ application/api/extra/wx.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Li Ning
- * Date: 2018/5/25
- * Time: 10:59
- */
+
 return [
     'app_id'=>'wx7195f81fc31b978f',
     'app_secret'=>'c14403fc7df85180c46c9dfd54089d28',
